6227aa138cabc05d34890c4c2175b7d20d6b3ef7
Sergio Gonzalez <d.chile58@live.fr>
2024-06-25 17:03:43 +0200

    Creation des routes /register et /login en cours. Petit soucis lors synchronisation lors du hashage du mot de passe, de l'ajout dans la bdd et de la réponse de la route. Correction à apporter sur /login lors de la comparaison des mots de passe.
    Co-authored-by: Valcasara Bryan <ValcasaraBryan@users.noreply.github.com>

--- authentification/app.js
+++ authentification/app.js
@@ -1,27 +1,85 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt')
 const app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+try {
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+} catch {
+    app.statusCode(400)
+    res.send({ statut: "Erreur", message: "JSON incorrect" });
+    return ;
+}
 
 const sqlite3 = require('sqlite3').verbose();
 
 // Connexion à la base de donnée.
-const db = new sqlite3.Database(':compte_itineraire:');
+const db = new sqlite3.Database('compte_itineraire');
+
+// Creation de la table compte si elle n'existe pas
+
+db.run("CREATE TABLE IF NOT EXISTS compte (\
+        identifiant VARCHAR(100) NOT NULL, \
+        motdepasse VARCHAR(255) NOT NULL, \
+        PRIMARY KEY (identifiant) )");
 
 // Serveur d'authentification.
 
+
+async function hashPassword (identifiant, password) {
+
+    const saltRounds = 10;
+  
+    const hashedPassword = await new Promise((resolve, reject) => {
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+        if (err) reject(err)
+        resolve(hash)
+      });
+    })
+  
+    return hashedPassword
+  }
+  
+
 // Création de compte. méthode POST
-app.get('/register', (req, res) => {
-    console.log(req.body);
-    res.send('ok');
+app.post('/register', async (req, res) => {
+    const identifiant = req.body.identifiant;
+    const motdepasse = req.body.motdepasse;
+    await hashPassword(motdepasse, 10, (err, hash) => {
+        sql = db.prepare("INSERT INTO compte VALUES (?, ?)");
+        sql.run(identifiant, hash)
+        sql.finalize();  
+        
+        db.all(`SELECT * from compte WHERE identifiant = "${identifiant}"`, function(err, row) {
+            console.log(row);
+        })
+    })
+    res.send('ok')
 })
 
 // Connexion. Méthode POST
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
+    const identifiant = req.body.identifiant;
+    const motdepasse = req.body.motdepasse;
+    console.log(`SELECT motdepasse FROM compte WHERE identifiant = '${identifiant}'`)
+    const hash = db.run(`SELECT motdepasse FROM compte WHERE identifiant = '${identifiant}'`)
+
+    console.log("hash", hash)
+    const result = bcrypt.compare(motdepasse, hash);
+    if (result) {
+
+        // TODO : Création du jeton
+        // TODO : Sauvegarde du jeton
+
+        res.statusCode("200");
+        res.send({ statut: "Succès", message: "JWT Token" });
+    } else {
+        res.statusCode("401");
+        res.send({ statut: "Erreur", message: "Identifiants incorrects" });
+    }
 })
 
 // Déconnexion. Méthode GET
@@ -31,11 +89,11 @@ app.get('/logout', (req, res) => {
 })
 
 // Vérification du jeton. Méthode POST
-app.get('/verify', (req, res) => {
+app.post('/verify', (req, res) => {
 })
 
 // Modification des données d'un compte. Méthode PATCH
-app.get('/update', (req, res) => {
+app.patch('/update', (req, res) => {
 })
 
 
