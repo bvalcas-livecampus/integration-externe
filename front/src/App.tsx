abf7ea928a77ebad113d67bedeed8013692a51e0
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-06-25 15:42:44 +0200

    initialisation de la map

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import L from "leaflet";
+import "leaflet-canvas-marker";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import 'leaflet/dist/leaflet.css';
+import './app.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MapContainer id="map" center={[51.505, -0.09]} zoom={10} scrollWheelZoom={false} preferCanvas={true} renderer={L.canvas()}>
+        <>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </>
+      </MapContainer>
     </>
   )
 }
