2d3bfcc4281f2d0a543730481dd02983dd24b252
Sergio Gonzalez <d.chile58@live.fr>
2024-06-26 14:25:50 +0200

    Completion des routes /register et /login
    Co-authored-by: Valcasara Bryan <ValcasaraBryan@users.noreply.github.com>

--- authentification/app.js
+++ authentification/app.js
@@ -3,83 +3,101 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt')
 const app = express();
 
+
+const sqlite3 = require('sqlite3').verbose();
+
+// Connexion à la base de donnée.
+
+
 try {
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
+    app.use((req, res, next) => {
+        // S'il y a déjà une variable req.db, on continue
+        // Il n'y a pas de raison.
+        if(req.db) {
+            next();
+        } else {
+            req.db = new sqlite3.Database('compte_itineraire');
+
+            
+            // Creation de la table compte si elle n'existe pas
+            req.db.run("CREATE TABLE IF NOT EXISTS compte (\
+                identifiant VARCHAR(100) NOT NULL, \
+                motdepasse VARCHAR(255) NOT NULL, \
+                PRIMARY KEY (identifiant) )");
+        
+            next();
+        }
+    });
 } catch {
-    app.statusCode(400)
+    app.status(400)
     res.send({ statut: "Erreur", message: "JSON incorrect" });
     return ;
 }
-
-const sqlite3 = require('sqlite3').verbose();
-
-// Connexion à la base de donnée.
-const db = new sqlite3.Database('compte_itineraire');
-
-// Creation de la table compte si elle n'existe pas
-
-db.run("CREATE TABLE IF NOT EXISTS compte (\
-        identifiant VARCHAR(100) NOT NULL, \
-        motdepasse VARCHAR(255) NOT NULL, \
-        PRIMARY KEY (identifiant) )");
-
-// Serveur d'authentification.
-
-
-async function hashPassword (identifiant, password) {
-
-    const saltRounds = 10;
-  
-    const hashedPassword = await new Promise((resolve, reject) => {
-      bcrypt.hash(password, saltRounds, function(err, hash) {
-        if (err) reject(err)
-        resolve(hash)
-      });
-    })
-  
-    return hashedPassword
-  }
   
 
 // Création de compte. méthode POST
 app.post('/register', async (req, res) => {
     const identifiant = req.body.identifiant;
     const motdepasse = req.body.motdepasse;
-    await hashPassword(motdepasse, 10, (err, hash) => {
-        sql = db.prepare("INSERT INTO compte VALUES (?, ?)");
-        sql.run(identifiant, hash)
-        sql.finalize();  
-        
-        db.all(`SELECT * from compte WHERE identifiant = "${identifiant}"`, function(err, row) {
-            console.log(row);
+    bcrypt.hash(motdepasse, 10, (err, hash) => {
+        if(err){
+            console.error('Une erreure est survenue lors du hachage. Veuillez contacter l\'administrateur. Error :' + err);
+            res.status(401);
+            res.send('Une erreure est survenue lors du hachage. Veuillez contacter l\'administrateur.')
+            return ;
+        }
+        let sql = req.db.prepare("INSERT INTO compte VALUES (?, ?)", [identifiant, hash])
+        sql.run((err) => {
+            if (err){
+                console.error('Une erreure est survenue lors de l\'a création du compte : ' + err);
+                res.status(401);
+                res.send('Une erreure est survenue lors de l\'a création du compte');
+                return ;
+            }
+            console.log("Compte enregistré");
+            sql.finalize();  
+            res.send('Compte enregistré')
         })
     })
-    res.send('ok')
 })
 
 // Connexion. Méthode POST
 app.post('/login', (req, res) => {
     const identifiant = req.body.identifiant;
     const motdepasse = req.body.motdepasse;
-    console.log(`SELECT motdepasse FROM compte WHERE identifiant = '${identifiant}'`)
-    const hash = db.run(`SELECT motdepasse FROM compte WHERE identifiant = '${identifiant}'`)
 
-    console.log("hash", hash)
-    const result = bcrypt.compare(motdepasse, hash);
-    if (result) {
+    let sql = req.db.prepare(`SELECT motdepasse FROM compte WHERE identifiant = ?`, [ identifiant ])
+    sql.get( async (err, result) => {
+        if (err) {
+            console.error('Erreur sql :', err);
+            res.status(400);
+            res.send('Erreur sql')
+            return ;
+        }
+        if(!result.motdepasse) {
+            console.error("Compte inconnu (ou en double, c'est un problème)");
+            res.status(401);
+            res.send("Compte inconnu (ou en double, c'est un problème)")
+            return ;
+        }
+        const row = result.motdepasse;
+        
+        const compare = await bcrypt.compare(motdepasse, row.toString());
+        if(compare !== true) {
+            console.error("Identifiants incorrects");
+            res.status(401);
+            res.send({ statut: "Erreur", message: "Identifiants incorrects" });
+            return ;
+        }
 
-        // TODO : Création du jeton
-        // TODO : Sauvegarde du jeton
-
-        res.statusCode("200");
+        console.error("JWT Token");
+        res.status(200)
         res.send({ statut: "Succès", message: "JWT Token" });
-    } else {
-        res.statusCode("401");
-        res.send({ statut: "Erreur", message: "Identifiants incorrects" });
-    }
+    })
 })
 
 // Déconnexion. Méthode GET
