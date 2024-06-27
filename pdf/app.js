b4191fa9102db5b8fabbe4df4d32ff8d4398de99
Sergio Gonzalez <d.chile58@live.fr>
2024-06-27 16:50:14 +0200

    partie pdf en cours de dev

---
+++ pdf/app.js
@@ -0,0 +1,164 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
+const PuppeteerHTMLPDF = require("puppeteer-html-pdf");
+
+const sqlite3 = require('sqlite3').verbose();
+
+// Connexion à la base de donnée.
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(cors())
+
+try {
+    app.use(bodyParser.json());
+} catch {
+    app.status(400)
+    res.send({ statut: "Erreur", message: "JSON incorrect" });
+    return ;
+}
+app.use((req, res, next) => {
+    // S'il y a déjà une variable req.db, on continue
+    // Il n'y a pas de raison.
+    if(req.db) {
+        next();
+    } else {
+        req.db = new sqlite3.Database('pdf');
+        
+        // Creation de la table compte si elle n'existe pas
+        req.db.run("CREATE TABLE IF NOT EXISTS pdf (\
+            id_itineraire integer NOT NULL, \
+            url VARCHAR(255) NOT NULL, \
+            status boolean NOT NULL,\
+            PRIMARY KEY (id_itineraire) )");
+    
+        next();
+    }
+});
+
+/**
+ * Cette fonction permet de récupérer les données de l'api adresse
+ * @param lon {number}
+ * @param lat {number}
+ * @return {Promise<
+*  {
+*   "type":"FeatureCollection",
+*   "version":"draft",
+*   "features":[
+*   {
+*       "type":"Feature",
+*       "geometry":{
+*           "type":"Point",
+*           "coordinates":[
+*               2.290084,
+*               49.897443
+*           ]
+*       },
+*       "properties":{
+*           "label":"8 Boulevard du Port 80000 Amiens",
+*           "score":0.49159121588068583,
+*           "housenumber":"8",
+*           "id":"80021_6590_00008",
+*           "type":"housenumber",
+*           "name":"8 Boulevard du Port",
+*           "postcode":"80000",
+*           "citycode":"80021",
+*           "x":648952.58,
+*           "y":6977867.25,
+*           "city":"Amiens",
+*           "context":"80, Somme, Hauts-de-France",
+*           "importance":0.6706612694243868,
+*           "street":"Boulevard du Port"
+*       }
+*   }
+*  } | Error>
+*/
+const api_adresse = async (lon, lat) => {
+    const response = await fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=${lon}&lat=${lat}`, {
+        method: "GET"
+    });
+    if (response.ok) {
+        const responseJson = await response.json();
+        return responseJson;
+    }
+    else {
+        const responseJson = await response.json();
+        throw new Error(responseJson.message);
+    }
+}
+
+
+// creation du pdf
+app.post('/itinerary', async (req, res) => {
+    const {itinerary, name, points} = req.body;
+    const htmlPDF = new PuppeteerHTMLPDF();
+    const url = `./public/` + itinerary + ' - ' + name + '.pdf';
+    const options = {
+        format: "A4",
+        path: url, 
+    };
+    htmlPDF.setOptions(options);
+
+    let sql = req.db.prepare("INSERT INTO pdf VALUES (?, ?, ?)", [itinerary, url, 0])
+    sql.run((err) => {
+        if (err){
+            console.error('Une erreure est survenue lors de l\'ajout du pdf dans la bdd : ' + err);
+            return ;
+        }
+        console.log("Pdf ajouté à la bdd");
+        sql.finalize();
+    })
+    
+    let content = "<h1>" + name + "</h1>"
+
+    res.status(204);
+    res.send();
+    
+    await Promise.all(points.map( async (coordinates) => {
+        const info = await api_adresse(coordinates["lon"], coordinates['lat'])
+        content = content + "<p>Aller à " + info.features[0].properties.street + "</p>";
+    }));
+
+    try {
+        await htmlPDF.create(content)
+        let sql = req.db.prepare("UPDATE pdf set status = 1 WHERE id_itineraire = ?", [itinerary])
+        sql.run((err) => {
+            if (err){
+                console.error('Une erreure est survenue lors de la maj du status du pdf dans la bdd : ' + err);
+                return ;
+            }
+            console.log("Status du pdj mis à jour");
+            sql.finalize();
+        })
+    } catch (error){
+        console.log("Erreur lors de la création de pdf : ", error)
+    }
+})
+
+// récupération du pdf
+app.get('/itinerary', async (req, res) => {
+    const {token, id} = req.body;
+
+    res.status(204);
+    res.send();
+    
+    await Promise.all(points.map( async (coordinates) => {
+        const info = await api_adresse(coordinates["lon"], coordinates['lat'])
+        content = content + "<p>Aller à " + info.features[0].properties.street + "</p>";
+    }));
+
+    try {
+        await htmlPDF.create(content)
+    } catch (error){
+        console.log("Erreur lors de la création de pdf : ", error)
+    }
+})
+
+
+var server = app.listen(3002, () => {
+    console.log("On écoute sur le port 3002");
+});
